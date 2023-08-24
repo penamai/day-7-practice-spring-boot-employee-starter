45f7a1e40684ee046c895ba9d2a9f4788b9ba639
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-24 09:53:54 +0800

    feat: added response status of BAD_REQUEST for Inactive and NotValid exceptions

--- src/main/java/com/thoughtworks/springbootemployee/exception/NotValidEmployeeAgeException.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/NotValidEmployeeAgeException.java
@@ -1,5 +1,9 @@
 package com.thoughtworks.springbootemployee.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotValidEmployeeAgeException extends RuntimeException{
     public NotValidEmployeeAgeException() {
         super("Employee must be 18~65 years old");
